460cf8c4ecf7e8b7a191b7ac4a8388bc4080229f
Leonardo <leoot2000@hotmail.com>
2023-06-14 20:14:52 -0300

    Feature / Remove Original Content
    Remove Original Content

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from pycaret.regression import *
 from load_data import load_data
 
 """
-# Welcome to Albedo Prediction App!
+# Predizendo Albedo de Asteróides com IA
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
@@ -20,27 +20,6 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 st.image("pictures/slide_1.png", caption="Slide 1")
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-                    .mark_circle(color='#0068c9', opacity=0.5)
-                    .encode(x='x:Q', y='y:Q'))
 
 columns_names = ["ASTEROID_NUMBER", "ASTEROID_NAME", "OCCULTATION_TIME",
                  "PHASE_ANGLE", "ECLIPTIC_LONGITUDE", "ECLIPTIC_LATITUDE",
